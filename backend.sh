fc88e9fd0420ba4ef4206c756736eeb1cf458c3c
Zaheer <thisiszaheer.786@gmail.com>
2024-03-24 16:38:51 +0530

    Written Backend Script for Expense Project

--- backend.sh
+++ backend.sh
@@ -11,6 +11,7 @@ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
+cd /app
 npm install
 
 cp backend.service /etc/systemd/system/backend.service
