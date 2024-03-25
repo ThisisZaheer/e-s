d6c92ea11ee66f527fba4d1c4c72fa702d964ff3
Zaheer <thisiszaheer.786@gmail.com>
2024-03-25 12:36:54 +0530

    Using Colour Code to the Script

--- backend.sh
+++ backend.sh
@@ -1,43 +1,43 @@
-echo Disable Default version of NodeJs
+echo -e "\e[33m Disable Default version of NodeJs\e[0m"
 dnf module disable nodejs -y
 
-echo Enable NodeJS18 Version
+echo -e "\e[33m Enable NodeJS18 Version\e[0m"
 dnf module enable nodejs:18 -y
 
-echo Install NodeJS
+echo -e "\e[33m Install NodeJS\e[0m"
 dnf install nodejs -y
 
-echo Configure Backend Service
+echo -e "\e[33m Configure Backend Service\e[0m"
 cp backend.service /etc/systemd/system/backend.service
 
-echo Adding Application User
+echo -e "\e[33m Adding Application User\e[0m"
 useradd expense
 
-echo Remove existing Application Content
+echo -e "\e[33m Remove existing Application Content\e[0m"
 rm -rf /app
 
-echo Create Application Directory
+echo -e "\e[33m Create Application Directory\e[0m"
 mkdir /app
 
-echo Download Application Content
+echo -e "\e[33m Download Application Content\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 
-echo Extract the Application Content
+echo -e "\e[33m Extract the Application Content\e[0m"
 unzip /tmp/backend.zip
 
-echo Download Application Dependencies
+echo -e "\e[33m Download Application Dependencies\e[0m"
 npm install
 
-echo Reloading SystemD Service
+echo -e "\e[33m Reloading SystemD Service\e[0m"
 systemctl daemon-reload
 
-echo Start Backend Service
+echo -e "\e[33m Start Backend Service\e[0m"
 systemctl enable backend
 systemctl restart backend
 
-echo Install Mysql Client
+echo -e "\e[33m Install Mysql Client\e[0m"
 dnf install mysql -y
 
-echo Load Schema
+echo -e "\e[33m Load Schema\e[0m"
 mysql -h mysql-dev.iamzaheer.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
