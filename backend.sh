a9cf09753b2a00f237719274b6e4dd329e843826
Zaheer <thisiszaheer.786@gmail.com>
2024-04-02 15:47:06 +0530

    Keeping DRY code

--- backend.sh
+++ backend.sh
@@ -1,46 +1,52 @@
 MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
 
-echo -e "\e[33m Disable Default version of NodeJs\e[0m"
+Head()  {
+
+  echo -e "\e[33m$1\e[0m"
+
+}
+
+Head "Disable Default version of NodeJs"
 dnf module disable nodejs -y &>>$log_file
 
-echo -e "\e[33m Enable NodeJS18 Version\e[0m"
+Head "Enable NodeJS18 Version"
 dnf module enable nodejs:18 -y &>>$log_file
 
-echo -e "\e[33m Install NodeJS\e[0m"
+Head "Install NodeJS"
 dnf install nodejs -y &>>$log_file
 
-echo -e "\e[33m Configure Backend Service\e[0m"
+Head "Configure Backend Service"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-echo -e "\e[33m Adding Application User\e[0m"
+Head "Adding Application User"
 useradd expense &>>$log_file
 
-echo -e "\e[33m Remove existing Application Content\e[0m"
+Head "Remove existing Application Content"
 rm -rf /app &>>$log_file
 
-echo -e "\e[33m Create Application Directory\e[0m"
+Head "Create Application Directory"
 mkdir /app &>>$log_file
 
-echo -e "\e[33m Download Application Content\e[0m"
+Head "Download Application Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app &>>$log_file
 
-echo -e "\e[33m Extract the Application Content\e[0m"
+Head "Extract the Application Content"
 unzip /tmp/backend.zip &>>$log_file
 
-echo -e "\e[33m Download Application Dependencies\e[0m"
+Head "Download Application Dependencies"
 npm install &>>$log_file
 
-echo -e "\e[33m Reloading SystemD Service\e[0m"
+Head "Reloading SystemD Service"
 systemctl daemon-reload &>>$log_file
 
-echo -e "\e[33m Start Backend Service\e[0m"
+Head "Start Backend Service"
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
 
-echo -e "\e[33m Install Mysql Client\e[0m"
+Head "Install Mysql Client"
 dnf install mysql -y &>>$log_file
 
-echo -e "\e[33m Load Schema\e[0m"
+Head "Load Schema"
 mysql -h mysql-dev.iamzaheer.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
