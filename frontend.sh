276d86f34a7e8c55b7892decea4e54d2c1fd85ba
Zaheer <thisiszaheer.786@gmail.com>
2024-04-03 11:22:05 +0530

    Written the echo command to show the headings of Script

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,21 @@
+echo Install Nginx Server
 dnf install nginx -y
 
+echo Configure frontend Service
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo Remove existing Application content
 rm -rf /usr/share/nginx/html/*
 
+echo Downloading the App content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
+
+echo Extracting the App content
 unzip /tmp/frontend.zip
 
+echo Enable & Start Nginx Service
 systemctl enable nginx
 systemctl restart nginx
 
