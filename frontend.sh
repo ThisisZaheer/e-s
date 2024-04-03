48e667a42ca5cfc9af43f641aeedd9cf5e3d4573
Zaheer <thisiszaheer.786@gmail.com>
2024-04-03 11:43:44 +0530

    Try to keep code DRY

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,25 @@
+log_file=/tmp/expense.log
+
 echo -e "\e[31m Install Nginx Server\e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+dnf install nginx -y &>>$log_file
 
 echo -e "\e[31m Configure frontend Service\e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo -e "\e[31m Remove existing Application content\e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 echo -e "\e[31m Downloading the App content\e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
 cd /usr/share/nginx/html
 
 echo -e "\e[31m Extracting the App content\e[0m"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>$log_file
 
 echo -e "\e[31m Enable the nginx Service\e[0m"
-systemctl enable nginx &>>/tmp/expense.log
+systemctl enable nginx &>>$log_file
 
 echo -e "\e[31m Restart the nginx service\e[0m"
-systemctl restart nginx &>>/tmp/expense.log
+systemctl restart nginx &>>$log_file
 
