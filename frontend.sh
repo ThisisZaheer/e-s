4a3c8b4fbb619ab14d34b7def9e0a3d1ecf9fe92
Zaheer <thisiszaheer.786@gmail.com>
2024-04-03 11:32:50 +0530

    Colour Code to the Script

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,23 @@
-echo Install Nginx Server
+echo -e "\e[31m Install Nginx Server\e[0m"
 dnf install nginx -y
 
-echo Configure frontend Service
+echo -e "\e[31m Configure frontend Service\e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo Remove existing Application content
+echo -e "\e[31m Remove existing Application content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo Downloading the App content
+echo -e "\e[31m Downloading the App content\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
 
-echo Extracting the App content
+echo -e "\e[31m Extracting the App content\e[0m"
 unzip /tmp/frontend.zip
 
-echo Enable the nginx Service
+echo -e "\e[31m Enable the nginx Service\e[0m"
 systemctl enable nginx
 
-echo Restart the nginx service
+echo -e "\e[31m Restart the nginx service\e[0m"
 systemctl restart nginx
 
