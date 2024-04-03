77040e328de498bb198eab82a020ca83f370fe97
Zaheer <thisiszaheer.786@gmail.com>
2024-04-03 11:48:01 +0530

    Try to keep code DRY

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,29 @@
 log_file=/tmp/expense.log
 
-echo -e "\e[31m Install Nginx Server\e[0m"
+Head() {
+  
+  echo -e "\e[31m$1\e[0m"
+}
+Head "Install Nginx Server"
 dnf install nginx -y &>>$log_file
 
-echo -e "\e[31m Configure frontend Service\e[0m"
+Head "Configure frontend Service"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
-echo -e "\e[31m Remove existing Application content\e[0m"
+Head "Remove existing Application content"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 
-echo -e "\e[31m Downloading the App content\e[0m"
+Head "Downloading the App content"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 
 cd /usr/share/nginx/html
 
-echo -e "\e[31m Extracting the App content\e[0m"
+Head "Extracting the App content"
 unzip /tmp/frontend.zip &>>$log_file
 
-echo -e "\e[31m Enable the nginx Service\e[0m"
+Head "Enable the nginx Service"
 systemctl enable nginx &>>$log_file
 
-echo -e "\e[31m Restart the nginx service\e[0m"
+Head "Restart the nginx service"
 systemctl restart nginx &>>$log_file
 
